def1dded3d2e09c06c30cdf1e577aaebab35e406
Benny Wong <benny@bwong.net>
2011-06-03 00:13:13 -0400

    Fix whitespace

--- js/jquery.location.plugin.js
+++ js/jquery.location.plugin.js
@@ -8,14 +8,14 @@
         var countryIsoField = $('<input></input>').attr('type', 'hidden').attr('name', 'country_iso').attr('id', 'country_iso');
         $(options.searchInput).after(cityWoeidField);
         $(options.searchInput).after(countryIsoField);
-        
+
         //Prepare the UI
         var div = $('<div></div>').addClass('location-results').attr('id', 'location-results');
         var header = $('<p></p>').html('Choose the correct location below...');
         var ul = $('<ul></ul>').attr('id', 'location-suggest');
         $(options.searchInput).parent().append(div.append(header).append(ul));
         $(options.resultCanvas).hide();
-        
+
         //Avoid submit the form when enter key pressed
         if($(options.searchInput).length > 0){
             $(options.searchInput).bind('keypress', function(event){
@@ -27,7 +27,7 @@
                 }
             });
         }
-        
+
         //Bind the search
         if($(options.searchButton).length > 0){
             $(options.searchButton).bind('click', function(e){
@@ -40,7 +40,7 @@
                 }
             });
         }
-        
+
         //Bind the blur event
         if($(options.searchInput).length > 0){
             $(options.searchInput).bind('blur', function(){
@@ -52,38 +52,38 @@
                     }
                 }
             });
-            
+
             $(options.searchInput).bind('change', function(){
                 $(options.searchInput).addClass('edited');
             });
         }
     };
-    
+
     //Geocode location with yahoo api
     $.fn.locationWidget.geocode = function(){
         if($.fn.locationWidget.searchInProgess){
             return false;
         }
-        
+
         options = $.fn.locationWidget.options;
-        
+
         $(options.searchInput).css('background', '#fff url(images/spinner.gif) 98% 50% no-repeat');
-        
+
         var url = 'http://where.yahooapis.com/v1/places$and(.q(' + escape($(options.searchInput).val()) + '),.type(7));count=0?format=json&lang=' + options.locale + '&appid=' + options.yahooApiKey + "&callback=?";
-        
+
         $.getJSON(url, function(data){
             if(data.places.total > 0){
                 $(options.searchInput).attr('readonly', 'readonly');
-                
+
                 $(options.resultList).html('');
                 $(data.places.place).each(function(i, e){
                     var name = e.name;
                     if(e.admin1 != e.name){
                         name += ', ' + e.admin1;
                     }
-                    
+
                     name += ', ' + e.country;
-                    
+
                     $(options.resultList).append($('<li></li>').append($('<a></a>').attr('href' , '#').html(name)).bind('click', function(event){
                         event.preventDefault();
                         $(options.cityWoeid).val(e.woeid);
@@ -91,34 +91,34 @@
                         $(options.searchInput).val($(this).children().html());
                         $(options.resultCanvas).hide();
                         $(options.searchInput).removeClass('edited');
-                        
+
                         $(options.searchInput).attr('readonly', '');
                         $(options.searchInput).blur();
-                        
+
                         $.fn.locationWidget.searchInProgess = false;
                     }));
                 });
-                
+
                 $(options.resultCanvas + ' p').html('Choose the correct location below...').removeClass('noresults');
                 $(options.resultList).show();
-                
+
                 $.fn.locationWidget.searchInProgess = true;
             }else{
                 $(options.resultCanvas + ' p').html('No results for <strong>' + $(options.searchInput).val() + '</strong>').addClass('noresults');
                 $(options.resultList).hide();
-                
+
                 $(options.searchInput).removeClass('edited');
-                
+
                 $.fn.locationWidget.searchInProgess = false;
             }
-            
+
             $(options.searchInput).css('background', '#FFF');
             $(options.resultCanvas).show();
         });
     };
-    
+
     $.fn.locationWidget.searchInProgess = false;
-    
+
     //Default options
     $.fn.locationWidget.options = {
         resultCanvas: '#location-results',
